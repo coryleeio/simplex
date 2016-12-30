f0fe586d8d58e7341afa96bfaa94b46f8866171f
Cory Lee <cl10001123@gmail.com>
2016-12-29 21:16:36 -0600

    Cleanup openGL attribute stuff

--- tests/10indexeddraws/10indexeddraws.cpp
+++ tests/10indexeddraws/10indexeddraws.cpp
@@ -10,7 +10,7 @@ int main() {
      0.5f, -0.5f,  0.0f,
     -0.5f, -0.5f,  0.0f
   };
-  
+
   // Create vao
   GLuint vao = 0;
   glGenVertexArrays(1, &vao);
@@ -22,28 +22,27 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, vbo); // set global variable 'GL_ARRAY_BUFFER' to vbo
   glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW); // write data into GL_ARRAY_BUFFER, that we just set
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);/* Copy into VAO: Specify that our coordinate data is going into attribute index 0, and contains three floats per vertex  */
-  glEnableVertexAttribArray(0); /* enables attribute 0 (see location 0) in the vertex shader.  If this wasn't called, attribute 0 can't be used for rendering.  It doesnt have to be done here, but it must be done before render */
+  glEnableVertexAttribArray(0); /* enables attribute 0 (see location 0) in the vertex shader.  If this wasn't called, attribute 0 can't be used for rendering.  It doesnt have to be done here, but it must be done before render*/
 
-  Simplex::Shader vertexShader("../tests/6translation/shader.vert", GL_VERTEX_SHADER);
-  Simplex::Shader fragmentShader("../tests/6translation/shader.frag", GL_FRAGMENT_SHADER);
+  Simplex::Shader vertexShader("../tests/10indexeddraws/shader.vert", GL_VERTEX_SHADER);
+  Simplex::Shader fragmentShader("../tests/10indexeddraws/shader.frag", GL_FRAGMENT_SHADER);
   Simplex::Program program(vertexShader, fragmentShader);
   program.activate();
    
   /* World translation matrix row-major */
   float input = 0.001f;
   Matrix4f gworld;
-  gworld.m[0][0] = 1.0f; gworld.m[0][1] = 0.0f; gworld.m[0][2] = 0.0f; gworld.m[0][3] = sinf(input);
-  gworld.m[1][0] = 0.0f; gworld.m[1][1] = 1.0f; gworld.m[1][2] = 0.0f; gworld.m[1][3] = 0.0f;
-  gworld.m[2][0] = 0.0f; gworld.m[2][1] = 0.0f; gworld.m[2][2] = 1.0f; gworld.m[2][3] = 0.0f;
-  gworld.m[3][0] = 0.0f; gworld.m[3][1] = 0.0f; gworld.m[3][2] = 0.0f; gworld.m[3][3] = 1.0f;
 
   while(!window.shouldClose()) {
     input += 0.0001f;
-	  gworld.m[0][3] = sinf(input);
+    gworld.m[0][0] = sinf(input); gworld.m[0][1] = 0.0f; gworld.m[0][2] = 0.0f; gworld.m[0][3] = 0.0f;
+    gworld.m[1][0] = 0.0f; gworld.m[1][1] = sinf(input); gworld.m[1][2] = 0.0f; gworld.m[1][3] = 0.0f;
+    gworld.m[2][0] = 0.0f; gworld.m[2][1] = 0.0f; gworld.m[2][2] = sinf(input); gworld.m[2][3] = 0.0f;
+    gworld.m[3][0] = 0.0f; gworld.m[3][1] = 0.0f; gworld.m[3][2] = 0.0f; gworld.m[3][3] = 1.0f;
     program.setMatrix4f("gWorld", gworld);
     input += 0.001f;
     window.clear();
-    // draw points 0-3 from the currently bound VAO with current in-use shader
+
     glDrawArrays(GL_TRIANGLES, 0, 3);
     // update other events like input handling  
     window.draw();
--- tests/8scale/8scale.cpp
+++ tests/8scale/8scale.cpp
@@ -11,17 +11,18 @@ int main() {
     -0.5f, -0.5f,  0.0f
   };
   
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
-
+  // Create vao
   GLuint vao = 0;
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
-  glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  // Create vbo to put in VAO
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo); // generate 1 buffer at location of vbo
+  glBindBuffer(GL_ARRAY_BUFFER, vbo); // set global variable 'GL_ARRAY_BUFFER' to vbo
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW); // write data into GL_ARRAY_BUFFER, that we just set
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);/* Copy into VAO: Specify that our coordinate data is going into attribute index 0, and contains three floats per vertex  */
+  glEnableVertexAttribArray(0); /* enables attribute 0 (see location 0) in the vertex shader.  If this wasn't called, attribute 0 can't be used for rendering.  It doesnt have to be done here, but it must be done before render*/
 
   Simplex::Shader vertexShader("../tests/8scale/shader.vert", GL_VERTEX_SHADER);
   Simplex::Shader fragmentShader("../tests/8scale/shader.frag", GL_FRAGMENT_SHADER);
@@ -42,7 +43,6 @@ int main() {
     program.setMatrix4f("gWorld", gworld);
     input += 0.001f;
     window.clear();
-    glBindVertexArray(vao);
     // draw points 0-3 from the currently bound VAO with current in-use shader
     glDrawArrays(GL_TRIANGLES, 0, 3);
     // update other events like input handling  
