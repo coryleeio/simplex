9b480feeacfb4140298aad3ecaffc7e53f5a0b03
Cory Lee <cory@visiblehealth.com>
2017-01-05 21:23:11 -0600

    Mac build fix

--- include/common.h
+++ include/common.h
@@ -2,6 +2,7 @@
 #define COMMON_H
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
+#include <math.h>
 #include <math3d.h>
 #include <stdio.h>
 #include <fstream>
